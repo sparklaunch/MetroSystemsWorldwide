7fce93d16acfd7d70faa463af3d6279a68596208
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-06-13 17:11:29 +0900

    Initial Commit

---
+++ MetroSystemsWorldwide/MetroSystemsWorldwideApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetroSystemsWorldwideApp.swift
+//  MetroSystemsWorldwide
+//
+//  Created by Jinwook Kim on 2023/06/13.
+//
+
+import SwiftUI
+
+@main
+struct MetroSystemsWorldwideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
